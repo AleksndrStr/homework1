245c6bb1eaa1d2a113f8e534cbb00014823dfd30
“AleksandrStr” <musicmakermusic@yandex.ru>
2025-03-15 20:22:22 +0300

    Финальная версия

--- hw-2.js
+++ hw-2.js
@@ -1,3 +1,54 @@
-let a = Number (20);
-alert (a);
+// let a = Number (20);
+// alert (a);
+
+// let year = Number (2007);
+// alert (year);
+
+// let name = 'Brendan Eich';
+// alert (name);
+
+// let b = Number (10);
+// let c = Number (2);
+// alert (b + c);
+// alert (b - c);
+// alert (b * c);
+// alert (b / c);
+// alert (c**5);
+
+// let a = Number (9);
+// let b = Number (2);
+// alert (a%b);
+
+// let num = 1;
+// num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num++;
+// num--;
+// alert(num)
+
+// let age = prompt ("Сколько вам лет?");
+// alert (age);
+
+// const user = {
+//     name: 'Sasha',
+//     age: 40,
+//     "isAdmin": true
+//  };
+
+//  let name = prompt ("Привет, ИМЯ");
+//  alert (name);
+
+// // Дополнительное задание:
+
+// let number = prompt("Загадай любое число");
+// alert (number);
+// let num = 5;
+// num *= 2;
+// num += 10;
+// num /= 2;
+// num -= 5;
+// alert (num);
+
 
